54dcdb389b2d497bd2f9f98490a6cb4bd9fc76cc
Samuron <samuronchik@gmail.com>
2015-04-30 08:45:51 +0300

    Getting familiar with F#

---
+++ Sources/Services/Equation/FinancialForecasting.Equation.Service/Library1.fs
@@ -0,0 +1,32 @@
+﻿namespace FinancialForecasting.Equation.Service
+
+open LMDotNet
+
+type EquationNode = 
+    | EnabledNode
+    | DisabledNode
+    | YNode
+
+module Equation = 
+    let nodeCalculator node c p = 
+        match node with
+        | EnabledNode -> p * c
+        | DisabledNode -> 0.0
+        | YNode -> -c
+    
+    let equationBuilder weighter nodes indices parameters = List.map3 weighter nodes indices parameters |> List.sum
+    
+    let solve (nodes : EquationNode list) (indices : float [] list) initial = 
+        let findMin = 
+            LMA.defaultSettings
+            |> LMA.init
+            |> LMA.minimize
+        
+        let calculate row parameters = 
+            equationBuilder nodeCalculator nodes row (List.ofArray parameters)
+        
+        let calcucalation (p : float []) (r : float []) = 
+            for i in 0..indices.Length - 1 do
+                r.[i] <- calculate (List.ofArray indices.[i]) p
+        
+        calcucalation |> findMin initial indices.Length
---
+++ Sources/Services/Equation/financialForecasting.Equation.Tests/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open FinancialForecasting.Equation.Service
+open FinancialForecasting.Migration
+open LMDotNet
+open System.ServiceModel
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let context = new FinancialForecastingContext()
+    let nodes = [ YNode; EnabledNode; EnabledNode; EnabledNode; EnabledNode; EnabledNode; EnabledNode ]
+    
+    let indices = 
+        context.EnterpriseIndices
+        |> Seq.map (fun index -> [| index.Y; index.X1; index.X2; index.X3; index.X4; index.X5; index.X6 |])
+        |> Seq.toList
+    
+    let initial = [| 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0 |]
+    let someshit = Equation.solve nodes indices initial
+    printfn "%s" someshit.Message
+    printfn "%g %g %g %g %g %g %g" someshit.OptimizedParameters.[0] someshit.OptimizedParameters.[1] 
+        someshit.OptimizedParameters.[2] someshit.OptimizedParameters.[3] someshit.OptimizedParameters.[4] 
+        someshit.OptimizedParameters.[5] someshit.OptimizedParameters.[6]
+    0 // return an integer exit code
